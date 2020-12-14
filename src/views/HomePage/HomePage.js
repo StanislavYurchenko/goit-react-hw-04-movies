efb670d702c0d706889c1cf51ce1e28482456898
Yurchenko Stanislav <yurchenko.stanislav@ukr.net>
2020-12-14 21:40:23 +0200

    ready

--- src/views/HomePage/HomePage.js
+++ src/views/HomePage/HomePage.js
@@ -14,15 +14,6 @@ function HomePage({ history, location }) {
 
   const currentPage = Number(parseQueryString(location.search).currentPage);
 
-  const getPopularMovie = () => {
-    setTimeout(() => {
-      popularFetch(currentPage).then(({ results, total_pages }) => {
-        setPopularList(results);
-        setTotalPages(total_pages);
-      });
-    }, 2000);
-  };
-
   const setCurrentPage = pageNumber => {
     history.push({
       ...location,
@@ -48,13 +39,17 @@ function HomePage({ history, location }) {
     .path;
 
   useEffect(() => {
-    console.log('useeffect');
+    const getPopularMovie = () => {
+      setTimeout(() => {
+        popularFetch(currentPage).then(({ results, total_pages }) => {
+          setPopularList(results);
+          setTotalPages(total_pages);
+        });
+      }, 2000);
+    };
+
     getPopularMovie();
-    history.push({
-      ...location,
-      search: `currentPage=${currentPage ? currentPage : 1}`,
-    });
-  }, [currentPage, history]);
+  }, [currentPage, history, location]);
 
   return (
     <section>
@@ -90,116 +85,6 @@ function HomePage({ history, location }) {
     </section>
   );
 }
-
-// class HomePage1 extends Component {
-//   state = {
-//     popularList: [],
-//     error: false,
-//     loading: false,
-//     currentPage: 1,
-//     total_pages: null,
-//   };
-
-//   componentDidMount = () => {
-//     this.getPopularMovie();
-//     const { history, location } = this.props;
-//     const { currentPage } = parseQueryString(location.search);
-
-//     history.push({
-//       ...location,
-//       search: `currentPage=${+currentPage ? +currentPage : 1}`,
-//     });
-//   };
-
-//   componentDidUpdate = (prevProps, prevState) => {
-//     const { location } = this.props;
-//     const { currentPage: prevCurrentPage } = parseQueryString(
-//       prevProps.location.search,
-//     );
-//     const { currentPage: nextCurrentPage } = parseQueryString(location.search);
-
-//     if (prevCurrentPage !== nextCurrentPage) {
-//       this.getPopularMovie();
-//     }
-//   };
-
-//   getPopularMovie = () => {
-//     const { location } = this.props;
-//     const { currentPage } = parseQueryString(location.search);
-//     popularFetch(+currentPage).then(({ results, total_pages }) =>
-//       this.setState({ popularList: results, total_pages: total_pages }),
-//     );
-//   };
-
-//   increaseCurrentPage = (number = 1) => {
-//     const { history, location } = this.props;
-//     const { currentPage } = parseQueryString(location.search);
-//     history.push({
-//       ...location,
-//       search: `currentPage=${+currentPage + number}`,
-//     });
-//   };
-
-//   decreaseCurrentPage = (number = 1) => {
-//     const { history, location } = this.props;
-//     const { currentPage } = parseQueryString(location.search);
-//     history.push({
-//       ...location,
-//       search: `currentPage=${+currentPage + number}`,
-//     });
-//   };
-
-//   setCurrentPage = (number = 1) => {
-//     const { history, location } = this.props;
-//     history.push({
-//       ...location,
-//       search: `currentPage=${number}`,
-//     });
-//   };
-
-//   render() {
-//     const { popularList } = this.state;
-//     const { location } = this.props;
-//     const { currentPage } = parseQueryString(location.search);
-
-//     const pathToMoviePage = routesMain.find(
-//       route => route.label === 'MoviesPage',
-//     ).path;
-
-//     return (
-//       <section>
-//         <h2>HomePage</h2>
-//         {popularList.length > 0 && (
-//           <>
-//             <ul>
-//               {popularList.map(({ id, original_title, original_name }) => {
-//                 return (
-//                   <li key={id}>
-//                     <Link
-//                       to={{
-//                         pathname: `${pathToMoviePage}/${id}`,
-//                         state: { from: location },
-//                       }}
-//                     >
-//                       {original_title || original_name}
-//                     </Link>
-//                   </li>
-//                 );
-//               })}
-//             </ul>
-//             <MoviePagination
-//               increaseCurrentPage={this.increaseCurrentPage}
-//               decreaseCurrentPage={this.decreaseCurrentPage}
-//               setCurrentPage={this.setCurrentPage}
-//               currentPage={+currentPage}
-//               total_pages={this.state.total_pages}
-//             />
-//           </>
-//         )}
-//       </section>
-//     );
-//   }
-// }
 
 // HOW TO DO THIS CORRECT??????????????
 HomePage.propTypes = {
